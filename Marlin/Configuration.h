12066ad305139588592a5c8b8c047fae0e75fcfa
Brandon Bowles (BranMac3) <brandonbowles@gmail.com>
2016-07-15 11:43:29 -0700

    Switch to use NTC 3950 thermistor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -129,7 +129,7 @@
 // 147 is Pt100 with 4k7 pullup
 // 110 is Pt100 with 1k pullup (non standard)
 
-#define TEMP_SENSOR_0 1    //robo
+#define TEMP_SENSOR_0 8    //NTC 3950
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_BED 1   //robo
